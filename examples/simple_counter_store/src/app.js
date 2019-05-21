e1ea07bd74e9211ec307c43802dcc66a7ec736d6
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-05-21 02:02:11 -0600

    add simplel counter w/ store example

---
+++ examples/simple_counter_store/src/app.js
@@ -0,0 +1,85 @@
+import {html, render} from '../../../node_modules/lit-html/lit-html.js';
+import createStore from '../../../lif/store.js';
+import connect from '../../../lif/stateless_store.js';
+
+let store = createStore({count: 10, type: 'DEC'});
+
+store.register('INC', ({state}) => ({count: state.count + 1}));
+store.register('DEC', ({state}) => ({count: state.count - 1}));
+store.register('TYP', (_, type) => ({type}));
+
+connect(
+  'counter-buttons',
+  store => _ => html`
+    <button
+      @click=${() => store.send('INC')}
+    >
+      inc
+    </button>
+    <button
+      @click=${() => store.send('DEC')}
+    >
+      dec
+    </button>
+    <button
+      @click=${() => store.reset()}
+    >
+      reset
+    </button>
+  `,
+  store
+);
+
+connect(
+  'counter-display-type',
+  store => _ => html`
+    <select
+      @change=${e => store.send('TYP', e.target.value)}
+    >
+      ${['DEC', 'BIN'].map(type => html`
+        <option
+          .selected=${store.state.type === type}
+          value=${type}
+        >
+          ${type}
+        </option>
+      `)}
+    </select>
+  `,
+  store
+);
+
+function display ({count, type}) {
+  switch (type) {
+    case 'BIN':
+      return (count >>> 0).toString(2);
+    case 'DEC':
+      return count;
+    default:
+      return 'N/A';
+  }
+}
+
+connect(
+  'counter-display',
+  store => _ => html`
+    <style>
+      code {
+        font-family: Consolas, monospace;
+        font-size: 2em;
+      }
+    </style>
+    <code>(${display(store.state)})</code>
+    <pre>${JSON.stringify(store.state, null, 2)}</pre>
+  `,
+  store
+);
+
+render(
+  html`
+    <counter-buttons></counter-buttons>
+    <counter-display-type></counter-display-type>
+    <counter-display></counter-display>
+  `,
+  document.getElementById('app')
+);
