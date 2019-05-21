853148b2051074a3d6716d5878d285992af7a0dd
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-05-21 02:03:46 -0600

    add so ref link

--- examples/simple_counter_store/src/app.js
+++ examples/simple_counter_store/src/app.js
@@ -52,6 +52,7 @@ connect(
 function display ({count, type}) {
   switch (type) {
     case 'BIN':
+      // @see https://stackoverflow.com/a/16155417/1858091
       return (count >>> 0).toString(2);
     case 'DEC':
       return count;
