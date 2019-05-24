0dab7f64aa3380b679e64d4897a49c53ad508221
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-05-23 20:31:26 -0600

    update stateless store so state is always in sync

--- lif/stateless_store.js
+++ lif/stateless_store.js
@@ -10,56 +10,12 @@ export default function (name, component, store) {
   customElements.define(name, class extends Base {
     constructor () {
       super();
-
-      this._component = component(store);
-
       store.subscribe(this.render.bind(this));
     }
 
     render () {
-      const res = this._component(this._props, this._shadowRoot);
+      const res = component(store)(this._props)
       render(res, this._shadowRoot);
     }
   });
 };
-
-
-// Anyway to re-use stateless class?
-
-/*
-picturing something like...
-
-function MyStatelessStore (store) {
-  return function (props) {
-    return html`
-      <button
-        @click=${store.send('INC_COUNTER')}
-      >
-        {props.text}
-      </button>
-      <span>${store.state.count}</span>
-    `;
-  }
-}
-
-or...
-
-function MyStatelessStore (props, store) {
-  return html`
-    <button
-      @click=${store.send('INC_COUNTER')}
-    >
-      {props.text}
-    </button>
-    <span>${store.state.count}</span>
-  `;
-}
-
----
-
-
-statelessStore('my-name', MyStatelessStore, myStore);
-
-// Maybe subclass stateless.js class; then just hook into
-// pub/sub on store instance, then trigger render method...
-*/
